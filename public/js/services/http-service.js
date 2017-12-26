05e1a08413cbaf0493b7387ef932f72fbbd2f88d
mgrinko <glukym@gmail.com>
2017-12-26 18:35:20 +0200

    API_URL was added

--- public/js/services/http-service.js
+++ public/js/services/http-service.js
@@ -1,8 +1,11 @@
+// const API_URL = 'http://localhost:3000';
+const API_URL = 'https://mgrinko.github.io/js2017-10-27';
+
 const HttpService = {
   get(url, successCallback) {
     let xhr = new XMLHttpRequest();
 
-    xhr.open('GET', url, true);
+    xhr.open('GET', API_URL + url, true);
 
     xhr.send();
     console.log('request was sent');
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 let http = require('http');
 var static = require('node-static');
 var file = new static.Server('.', {
-  cache: 0
+  cache: 0,
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': 'GET,PUT',
+  //   'Access-Control-Allow-Headers': 'Content-Type'
+  // }
 });
 
 function accept(req, res) {
